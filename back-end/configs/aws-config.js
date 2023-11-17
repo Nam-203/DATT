b8743ae7c7d5dd696bb4b94be7d4f3873f504bf2
TuyenNedd <trituyen2003@gmail.com>
2023-11-17 23:43:48 +0700

    upload user router

--- back-end/configs/aws-config.js
+++
@@ -1,6 +0,0 @@
-const awsConfig = {
-  accessKeyId: process.env.ACCESS_KEY_ID,
-  secretAccessKey: process.env.SECRET_ACCESS_KEY,
-};
-
-module.exports = { awsConfig };
--- back-end/routes/admin/router.user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../../../../../../Downloads/Duanbe_CODEGOC/Duanbe_CODEGOC/app/controllers/controller.user');
-
-router.get('/list', userController.userGetList);
-router.get('/authorize/:id', userController.userAuthorize);
-router.get('/unauthorize/:id', userController.userUnAuthorize);
-
-module.exports = router;

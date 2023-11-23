aaeb707bdc1a3490790e41b6b9b11fdc44575787
Chuong Nguyen <chuongsanji@gmail.com>
2023-11-23 22:27:43 +0700

    update ui

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from "react-dom";
+import App from "./app";
+import { store } from "./redux";
+import { Provider } from "react-redux";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import "./fonts/Lato-Italic.ttf";
+const queryClient = new QueryClient();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </QueryClientProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-

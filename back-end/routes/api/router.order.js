512d3ea5f56fa75c9fd7ad9715e2eda578c3ea8d
huutrung23 <151532351+huutrung23@users.noreply.github.com>
2023-11-23 21:44:25 +0700

    Delete back-end directory

--- back-end/routes/api/router.order.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express').Router();
-const OrderController = require('../../app/controllers/controller.order');
-
-Router.post('/create', OrderController.createOrder);
-Router.get('/allOrder', OrderController.getAllOrders);
-Router.put('/update/:id', OrderController.updateOrderById);
-Router.delete('/delete/:id', OrderController.deleteOrderById);
-Router.get('/:id', OrderController.GetOderById);
-module.exports = Router;

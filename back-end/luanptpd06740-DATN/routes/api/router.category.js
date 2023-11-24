992b426220548d351e8a5f9a206028f3dbb8d8b0
LuanSadBOiz <112767596+LuanSadBOiz@users.noreply.github.com>
2023-11-24 09:09:58 +0700

    Delete back-end/luanptpd06740-DATN directory

--- back-end/luanptpd06740-DATN/routes/api/router.category.js
+++
@@ -1,7 +0,0 @@
-const Router = require('express').Router();
-const categoryController = require('../../app/controllers/controller.category');
-
-Router.get('/', categoryController.index);
-Router.get('/:id', categoryController.detail);
-
-module.exports = Router;
